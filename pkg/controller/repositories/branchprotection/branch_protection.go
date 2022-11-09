bdba48356590b92c626d902ae4e60785a5ab7b0d
Feggah <gabidferreira9@gmail.com>
2022-11-09 12:33:21 -0300

    Add Observe method implementation
    Signed-off-by: Feggah <gabidferreira9@gmail.com>

--- pkg/controller/repositories/branchprotection/branch_protection.go
+++ pkg/controller/repositories/branchprotection/branch_protection.go
@@ -16,7 +16,9 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/pkg/errors"
+	"github.com/shurcooL/githubv4"
 	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -35,7 +37,10 @@ import (
 )
 
 const (
-	errUnexpectedObject = "The managed resource is not a BranchProtectionRule resource"
+	errUnexpectedObject          = "The managed resource is not a BranchProtectionRule resource"
+	errCheckBranchProtectionRule = "Cannot check if GitHub BranchProtectionRule exists"
+	errGetBranchProtectionRule   = "Cannot get GitHub BranchProtectionRule"
+	errCheckUpToDate             = "unable to determine if external resource is up to date"
 )
 
 // SetupBranchProtectionRule adds a controller that reconciles BranchProtectionRule.
@@ -98,12 +103,44 @@ type external struct {
 }
 
 func (e *external) Observe(ctx context.Context, mgd resource.Managed) (managed.ExternalObservation, error) {
-	_, ok := mgd.(*v1alpha1.BranchProtectionRule)
+	cr, ok := mgd.(*v1alpha1.BranchProtectionRule)
 	if !ok {
 		return managed.ExternalObservation{}, errors.New(errUnexpectedObject)
 	}
 
-	return managed.ExternalObservation{}, nil
+	isCreated, err := e.CheckBranchProtectionRuleExistance(ctx, cr)
+	if err != nil {
+		return managed.ExternalObservation{}, errors.Wrap(err, errCheckBranchProtectionRule)
+	}
+
+	if !isCreated {
+		return managed.ExternalObservation{
+			ResourceExists: false,
+		}, nil
+	}
+
+	external, err := e.GetBranchProtectionRule(ctx, cr.Status.AtProvider.ID)
+	if err != nil {
+		return managed.ExternalObservation{}, errors.Wrap(err, errGetBranchProtectionRule)
+	}
+
+	currentSpec := cr.Spec.ForProvider.DeepCopy()
+	branchprotection.LateInitialize(&cr.Spec.ForProvider, external)
+	lateInitialized := !cmp.Equal(currentSpec, &cr.Spec.ForProvider)
+
+	cr.Status.SetConditions(xpv1.Available())
+
+	diff, err := branchprotection.IsUpToDate(cr.Spec.ForProvider, external)
+	if err != nil {
+		return managed.ExternalObservation{}, errors.Wrap(err, errCheckUpToDate)
+	}
+
+	return managed.ExternalObservation{
+		ResourceExists:          true,
+		ResourceUpToDate:        diff == "",
+		ResourceLateInitialized: lateInitialized,
+		Diff:                    diff,
+	}, nil
 }
 
 func (e *external) Create(ctx context.Context, mgd resource.Managed) (managed.ExternalCreation, error) {
@@ -137,4 +174,64 @@ func (e *external) Delete(ctx context.Context, mgd resource.Managed) error {
 	fmt.Println("DELETE BRANCHPROTECTIONRULE")
 
 	return nil
+}
+
+// CheckBranchProtectionRuleExistance checks if a BranchProtectionRule pattern
+// exists in the desired repository.
+func (e *external) CheckBranchProtectionRuleExistance(ctx context.Context, cr *v1alpha1.BranchProtectionRule) (bool, error) {
+	var query struct {
+		Repository struct {
+			ID                    githubv4.String `graphql:"id"`
+			BranchProtectionRules struct {
+				Nodes []struct {
+					Pattern githubv4.String `graphql:"pattern"`
+					ID      githubv4.String `graphql:"id"`
+				} `graphql:"nodes"`
+			} `graphql:"branchProtectionRules(first: 100)"`
+		} `graphql:"repository(owner: $owner, name: $name)"`
+	}
+
+	if cr.Spec.ForProvider.Repository == nil {
+		return false, errors.New("required spec.forProvider.repository field is empty")
+	}
+
+	variables := map[string]interface{}{
+		"owner": githubv4.String(cr.Spec.ForProvider.Owner),
+		"name":  githubv4.String(*cr.Spec.ForProvider.Repository),
+	}
+
+	if err := e.gh.Query(ctx, &query, variables); err != nil {
+		return false, err
+	}
+
+	cr.Spec.ForProvider.RepositoryID = (*string)(&query.Repository.ID)
+
+	for _, node := range query.Repository.BranchProtectionRules.Nodes {
+		if node.Pattern == githubv4.String(cr.Spec.ForProvider.Pattern) {
+			cr.Status.AtProvider.ID = string(node.ID)
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+// GetBranchProtectionRule fetches the state of the desired
+// BranchProtectionRule in GitHub
+func (e *external) GetBranchProtectionRule(ctx context.Context, id string) (branchprotection.ExternalBranchProtectionRule, error) {
+	var query struct {
+		Node struct {
+			Node branchprotection.ExternalBranchProtectionRule `graphql:"... on BranchProtectionRule"`
+		} `graphql:"node(id: $id)"`
+	}
+
+	variables := map[string]interface{}{
+		"id": githubv4.ID(id),
+	}
+
+	if err := e.gh.Query(ctx, &query, variables); err != nil {
+		return branchprotection.ExternalBranchProtectionRule{}, err
+	}
+
+	return query.Node.Node, nil
 }
